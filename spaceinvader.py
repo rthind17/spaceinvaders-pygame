ddc633eee08e3d1157924124e7ccce6d35e02cf3
RTHIND <thind_rajan@outlook.com>
2019-08-29 13:44:09 -0400

    created the main screen of the game

---
+++ spaceinvader.py
@@ -0,0 +1,40 @@
+import pygame
+import sys
+from settings import *
+
+class Space_Invaders(object):
+    def __init__(self):
+        self.main_screen = main_screen
+        self.caption = pygame.display.set_caption('Space Invaders')
+        self.clock = pygame.time.Clock()
+        self.caption = pygame.display.set_caption('SpaceInvaders')
+        self.running = True
+        self.state = 'intro'
+
+    def run(self):
+        while self.running:
+            if self.state == 'intro':
+                self.intro_events()
+                self.intro_updates()
+                self.intro_draw()
+                
+            self.clock.tick(FPS)
+        pygame.quit()
+        sys.exit()
+
+   
+
+
+    
+
+
+
+
+
+
+
+
+
+
+        
+        
